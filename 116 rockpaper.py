0cc656b7a3e78d59c7fb1ff2d03a4bc7610f3342
rahul0121997 <rahul0121997@gmail.com>
2025-08-26 17:48:28 +0530

    final version

--- 116 rockpaper.py
+++ 116 rockpaper.py
@@ -1,21 +1,32 @@
-#rock paper scissors
 import random
 
-chances = int(input('enter the number of round you want to play: '))
+choices = ('paper','rock','scissors')
+computer_choice = random.choice(choices)
 
-while chances > 0:
-    choices = ('rock','paper', 'scissors')
-    user_choice = int(input("""enter your choice: 
-                            1 - rock
-                            2 - paper
-                            3 - scissors
-                            """))
+number_of_round = int(input("enter the number of round you want play: "))
 
-    computer_choice = random.choices(choices)
+computer_win = 0
+user_win = 0
+tie = 0
+while number_of_round > 0:
+    user_choice = input("enter you choice from(rock,paper,scissors): ")
+    print(computer_choice)
     
-    if user_choice == "paper" and computer_choice == "rock" or user_choice == "rock" and computer_choice == "scissors" or user_choice == "scissors" and computer_choice == "paper":
-        print("you win!!!")
+    if user_choice == computer_choice:
+        tie += 1
+        print("it's tieee!!!")
+    elif user_choice == 'rock' and computer_choice == 'scissors' or user_choice == 'paper' and computer_choice == 'rock' or user_choice == 'scissors' and computer_choice == 'paper':
+        print("you win!!!!")
+        user_win += 1
     else:
-        print("computer win!!")
-        
-    chances -= 1   
+        print("computer win!!!")
+        computer_win += 1
+    number_of_round -= 1
+    
+
+if computer_win > user_win:
+    print(f"computer is overall winner with won {computer_win}")
+elif user_win > computer_win:
+    print(f"you won overall with number of round:{user_win}")
+else:
+    print("it's tiee")
